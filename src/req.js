964cec4270fabb54dbf28ea7cdad46f8ac3a39b6
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-05-15 12:40:24 -0400

    Fix async/await

--- src/req.js
+++ src/req.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const v = require('./var.json')
 const axios = require('axios')
 const logData = require('./log')
@@ -6,19 +8,18 @@ const auth = require('./auth')
 module.exports = {
 
   upload: async function (id, token, estudio) {
-    let url = `${v.host}/api/usuarios/${id}/estudios?access_token=${token}`
 
-    await axios.post(url, {
-      data: estudio
-    })
-    .then(function (response) {
+    try {
+      logData.info('~$termodaq-api:', 'DATAFILE SUBIENDO AL SERVIDOR')
+      let url = `${v.host}/api/usuarios/${id}/estudios?access_token=${token}`
+      await axios.post(url, {
+        data: estudio
+      })
       logData.info('~$termodaq-api:', 'DATAFILE SUBIDO AL SERVIDOR')
       auth.logout(token)
-    })
-    .catch(function (e) {
-      logData.error('~$termodaq-api:', 'ERROR SUBIENDO ESTUDIO => ' + e)
+    } catch (e) {
+      logData.error('~$termodaq-api:', 'ERROR SUBIENDO DATAFILE => ' + e)
       auth.logout(token)
-    })
+    }
   }
-
 }
