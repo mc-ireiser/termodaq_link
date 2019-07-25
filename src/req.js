a45c6cbcd78687d2dcd316687375bf96160f6c1f
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-07-25 01:02:12 -0400

    Creacion de ficha

--- src/req.js
+++ src/req.js
@@ -7,16 +7,24 @@ const auth = require('./auth')
 
 module.exports = {
 
-  upload: async function (id, token, estudio) {
+  upload: async function (id, token, estudio, titulo, lugar) {
 
     try {
       logData.info('~$termodaq-api:', 'DATAFILE SUBIENDO AL SERVIDOR')
-      let url = `${v.host}/api/usuarios/${id}/estudios?access_token=${token}`
-      await axios.post(url, {
+      let url = `${v.host}/api/usuario/${id}/estudio?access_token=${token}`
+      const result = await axios.post(url, {
         data: estudio
       })
+      
       logData.info('~$termodaq-api:', 'DATAFILE SUBIDO AL SERVIDOR')
+      let urlFicha = `${v.host}/api/estudio/${result.data.id}/ficha?access_token=${token}`
+      const result2 = await axios.post(urlFicha, {
+        'titulo': titulo,
+        'lugar': lugar || 'N/A'
+      })
+
       auth.logout(token)
+
     } catch (e) {
       logData.error('~$termodaq-api:', 'ERROR SUBIENDO DATAFILE => ' + e)
       auth.logout(token)
